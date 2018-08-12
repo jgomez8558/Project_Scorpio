09bea6f12cf21a0c2fb390de98ac9a6af0fca11c
Jon Gomez <jgomez8558@gmail.com>
2018-08-12 14:51:22 -0500

    Adding PHP files to the repo

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+
+
+	$link = mysqli_connect("localhost", "root", "","users");
+//(nameOfHost,"userName","passwordOfDatabase","NameofDataBase")
+	if(mysqli_connect_error())
+	{
+		die("ERROR!");
+	}
+
+    echo "connected";
+
+
+?>
