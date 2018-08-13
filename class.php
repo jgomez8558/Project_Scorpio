22193acb806f1d5145113191367f2e3715af5f18
Unknown <seanfoytek@gmail.com>
2018-08-13 16:26:10 -0500

    need to fix and add js

--- class.php
+++ class.php
@@ -1,5 +1,19 @@
-<app-navbar></app-navbar>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Page Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="class.js"></script>
+</head>
 <body>
+  
+
+
+
 <section id="carousel">
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
@@ -112,5 +126,6 @@
       </div>
     
     </div>
- 
+
 </body>
+</html>
