2006912d1d10fe86ab8110d0278415ac1846b328
Unknown <seanfoytek@gmail.com>
2018-08-14 10:29:56 -0500

    stage class

---
+++ profile.php
@@ -0,0 +1,78 @@
+<?php
+include('connection.php');
+?>
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Profile</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+            <link rel="stylesheet" href="profile.css"/>
+        </head>
+        <body>
+        <div class="container-fluid p-0">
+  <div class="row">
+    <div class="col">
+      <img src="https://dz2cdn4.dzone.com/storage/rc-covers/8757499-recardheader90.png" id="background">
+      <img src="https://img.deusm.com/darkreading/DR-user.jpg" id="user">
+      <h3 class="name">UserName</h3>
+    </div>
+  </div>
+  <div class="row mb-5">
+    <div class="col-lg-8 col-md-8 col-sm-12 p-0">
+      <div class="card classes">
+        <div class="card-body">
+          <h5 class="card-title">Classes</h5>
+        </div>
+        <div class="card m-3">
+          <div class="card-body">
+            <h5 class="card-title">Class Name</h5>
+            <h6 class="card-subtitle mb-2 text-muted">MM/DD/YYYY</h6>
+            <p class="card-text">Wrote water woman of heart it total other. By in entirely securing suitable graceful at families improved. Zealously few furniture repulsive was agreeable consisted difficult. Collected breakfast estimable questions in to favourite it.</p>
+            <a href="#" class="card-link">Register</a>
+          </div>
+        </div>
+        <div class="card m-3">
+          <div class="card-body">
+            <h5 class="card-title">Class Name</h5>
+            <h6 class="card-subtitle mb-2 text-muted">MM/DD/YYYY</h6>
+            <p class="card-text">Wrote water woman of heart it total other. By in entirely securing suitable graceful at families improved. Zealously few furniture repulsive was agreeable consisted difficult. Collected breakfast estimable questions in to favourite it.</p>
+            <a href="#" class="card-link">Register</a>
+          </div>
+        </div>
+        <div class="card m-3">
+          <div class="card-body">
+            <h5 class="card-title">Class Name</h5>
+            <h6 class="card-subtitle mb-2 text-muted">MM/DD/YYYY</h6>
+            <p class="card-text">Wrote water woman of heart it total other. By in entirely securing suitable graceful at families improved. Zealously few furniture repulsive was agreeable consisted difficult. Collected breakfast estimable questions in to favourite it.</p>
+            <a href="#" class="card-link">Register</a>
+          </div>
+        </div>
+        <div class="card m-3">
+          <div class="card-body">
+            <h5 class="card-title">Class Name</h5>
+            <h6 class="card-subtitle mb-2 text-muted">MM/DD/YYYY</h6>
+            <p class="card-text">Wrote water woman of heart it total other. By in entirely securing suitable graceful at families improved. Zealously few furniture repulsive was agreeable consisted difficult. Collected breakfast estimable questions in to favourite it.</p>
+            <a href="#" class="card-link">Register</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-12 p-0">
+      <div class="card about">
+        <div class="card-body">
+          <h5 class="card-title">About UserName</h5>
+          <p>FirstName LastName</p>
+          <p>Address</p>
+          <p>Teacher at some school/place located somewhere</p>
+          <p>(123)456-7890</p>
+          <p>someone@somewhere.com</p>
+          <a href="setting.php"> <button type="button" class="btn btn-primary">Edit</button> </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        </body>
+</html>
--- registration.php
+++ registration.php
@@ -17,13 +17,12 @@ if(array_key_exists("submit", $_POST)){
     $dt = new DateTime();
     $currentTime =  $dt->format('Y-m-d H:i:s');
 
-
     // Adding User to the DataBase in profile table
-    $query = "INSERT INTO profile (firstName, lastName, address, email, phone, password) 
-                VALUES ('".mysqli_real_escape_string($link, $_POST['fname'])."', '".mysqli_real_escape_string($link, $_POST['lname'])."', '".mysqli_real_escape_string($link, $_POST['address'])."','".mysqli_real_escape_string($link, $_POST['email'])."', '".mysqli_real_escape_string($link, $_POST['phone'])."', '".mysqli_real_escape_string($link, $hash)."', '".mysqli_real_escape_string($link, FALSE)."')";
+    $query = "INSERT INTO profile (firstName, lastName, address, email, phone, role, password) 
+                VALUES ('".mysqli_real_escape_string($link, $_POST['fname'])."', '".mysqli_real_escape_string($link, $_POST['lname'])."', '".mysqli_real_escape_string($link, $_POST['address'])."','".mysqli_real_escape_string($link, $_POST['email'])."', '".mysqli_real_escape_string($link, $_POST['phone'])."', '".mysqli_real_escape_string($link, $_POST['role'])."', '".mysqli_real_escape_string($link, $hash)."')";
 
     // Connects and sends the query to the DataBase and goes to the login page
-    mysqli_query($link, $query);
+        mysqli_query($link, $query);
 
     if(isset($_POST['submit'])) {
 
@@ -42,8 +41,7 @@ if(array_key_exists("submit", $_POST)){
     <title>Register</title>
     <head>
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        
         <link rel="stylesheet" type="text/css" href="registration.css">
     </head>
 
@@ -58,13 +56,17 @@ if(array_key_exists("submit", $_POST)){
                      <input type="text" id="inputProfile" name = "address" class="form-control" placeholder="Address" required autofocus>
                     <input type="email" id="inputProfile" name = "email" class="form-control" placeholder="Email address" required autofocus>
                     <input type="text" id="inputProfile" name = "phone" class="form-control" placeholder="Phone" required autofocus>
+                    <input type="radio" checked="checked" name="role" value="1">Student<br>
+                    <input type="radio" name="role" value="2">Teacher
                     <input type="password" id="inputPassword" name = "password" class="form-control" placeholder="Password" required>
-                    <input class="btn btn-lg btn-primary btn-block btn-signin" type = "submit" name = "submit" id = "btn_sub" value = "Registeration">
+                    <input class="btn btn-lg btn-primary btn-block btn-signin" type = "submit" name = "submit" id = "btn_sub" value = "Registration">
                 </form><!-- /form -->
             </div><!-- /card-container -->
         </div><!-- /container -->
-        <script
-                src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
         <script src="myscripts.js"></script>
     </body>
 
