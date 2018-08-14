f3564094d46cc0dd1ba612cdb7282347dedce582
Jon Gomez <jgomez8558@gmail.com>
2018-08-14 10:31:19 -0500

    Add php functions in Home Page

--- home.php
+++ home.php
@@ -1,150 +1,182 @@
+<?php 
+
+session_start();
+
+include('connection.php');
+
+?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Title of the document</title>
+  <title>Home</title>
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
-        
-        <link rel="stylesheet" type="text/css" href="home.css">
-        <link rel="stylesheet" type="text/css" href="navbar.css">
-        <link rel="stylesheet" type="text/css" href="event.css">
+
+  <link rel="stylesheet" type="text/css" href="home.css">
+  <link rel="stylesheet" type="text/css" href="navbar.css">
+  <link rel="stylesheet" type="text/css" href="event.css">
 
 </head>
 
 <body>
-<nav>
-   <ul>
-    <li>Link 1</li>
-    <li>Link 2</li>
-    <li>Link 3</li>
-  </ul>
-</nav>
+  <nav>
+    <ul>
+      <li>
+        <a href="/braintrendy/logout.php">Logout</a>
+      </li>
+      <li>
+        <a href="/braintrendy/class.php">Classes</a>
+      </li>
+      <li>
+        <a href="/braintrendy/profile.php">Profile</a>
+      </li>
+    </ul>
+  </nav>
 
-<div class="banner"></div>
-<div id="body">
-<div id="hero-slides">
-  <div id="header">
-    <div id="logo"></div>
-    <div id="menu">
-      <div id="hamburger">
-        <div class="slice"></div>
-        <div class="slice"></div>
-        <div class="slice"></div>
+  <div class="banner"></div>
+  <div id="body">
+    <div id="hero-slides">
+      <div id="header">
+        <div id="logo"></div>
+        <div id="menu">
+          <div id="hamburger">
+            <div class="slice"></div>
+            <div class="slice"></div>
+            <div class="slice"></div>
+          </div>
+        </div>
+      </div>
+      <div id="slides-cont">
+        <div class="button" id="next"></div>
+        <div class="button" id="prev"></div>
+        <div id="slides">
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/ef5af971-fc46-4ee1-a589-980346478696_opt.png);">
+            <div class="number">01</div>
+            <div class="body">
+              <div class="location">Shibuya, Japan</div>
+              <div class="headline">Photo by Benjamin Hung</div>
+              <a href="https://unsplash.com/photos/EYmhcdGuYmI" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/522d6a86-0dcf-4554-8b22-7655d65a1f66_opt.png);">
+            <div class="number">02</div>
+            <div class="body">
+              <div class="location">Mong Kok, Hong Kong</div>
+              <div class="headline">Photo by Ryan Tang</div>
+              <a href="https://unsplash.com/photos/ANJHXftvvJ8" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/9b5f71cb-34a3-4fb4-a3a6-0ce9557c0acd_opt.png);">
+            <div class="number">03</div>
+            <div class="body">
+              <div class="location">Incheon, South Korea</div>
+              <div class="headline">Photo by Steve Roe</div>
+              <a href="https://unsplash.com/photos/73aocAAt7rs" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/10cfd52d-e217-41e9-bb8b-ad43384a5e63_opt.png);">
+            <div class="number">04</div>
+            <div class="body">
+              <div class="location">Wan Chai, Hong Kong</div>
+              <div class="headline">Photo by Sean Foley</div>
+              <a href="https://unsplash.com/photos/aPDCEoW7B78" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/b9669f2a-17fc-47dc-b6b5-3f863004efa8_opt.png);">
+            <div class="number">05</div>
+            <div class="body">
+              <div class="location">Shibuya-ku, Japan</div>
+              <div class="headline">Photo by Alex Knight</div>
+              <a href="https://unsplash.com/photos/Akz00I_GGjU" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/31979576-5060-4513-aae2-b379b87e7fe6_opt.png);">
+            <div class="number">06</div>
+            <div class="body">
+              <div class="location">Tokyo, Japan</div>
+              <div class="headline">Photo by Benjamin Hung</div>
+              <a href="https://unsplash.com/photos/pTn26knnKVw" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/429b83b8-1ad4-4450-b0de-0a0c1073cf1e_opt.jpg);">
+            <div class="number">07</div>
+            <div class="body">
+              <div class="location">Taipei City, Taiwan</div>
+              <div class="headline">Photo by Jesus In Taiwan</div>
+              <a href="https://unsplash.com/photos/v63B_MUiFw8" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div id="next-catch"></div>
+        <div id="prev-catch"></div>
+      </div>
+      <div id="footer">
+        <a href="" target="_blank">
+          <div id="dribbble"></div>
+        </a>
       </div>
     </div>
+
   </div>
-  <div id="slides-cont">
-    <div class="button" id="next"></div>
-    <div class="button" id="prev"></div>
-    <div id="slides">
-      <div class="slide" style="background-image: url(https://alca.tv/static/u/ef5af971-fc46-4ee1-a589-980346478696_opt.png);">
-        <div class="number">01</div>
-        <div class="body">
-          <div class="location">Shibuya, Japan</div>
-          <div class="headline">Photo by Benjamin Hung</div><a href="https://unsplash.com/photos/EYmhcdGuYmI" target="_blank">
-            <div class="link">View on Unsplash</div></a>
-        </div>
-      </div>
-      <div class="slide" style="background-image: url(https://alca.tv/static/u/522d6a86-0dcf-4554-8b22-7655d65a1f66_opt.png);">
-        <div class="number">02</div>
-        <div class="body">
-          <div class="location">Mong Kok, Hong Kong</div>
-          <div class="headline">Photo by Ryan Tang</div><a href="https://unsplash.com/photos/ANJHXftvvJ8" target="_blank">
-            <div class="link">View on Unsplash</div></a>
-        </div>
-      </div>
-      <div class="slide" style="background-image: url(https://alca.tv/static/u/9b5f71cb-34a3-4fb4-a3a6-0ce9557c0acd_opt.png);">
-        <div class="number">03</div>
-        <div class="body">
-          <div class="location">Incheon, South Korea</div>
-          <div class="headline">Photo by Steve Roe</div><a href="https://unsplash.com/photos/73aocAAt7rs" target="_blank">
-            <div class="link">View on Unsplash</div></a>
-        </div>
-      </div>
-      <div class="slide" style="background-image: url(https://alca.tv/static/u/10cfd52d-e217-41e9-bb8b-ad43384a5e63_opt.png);">
-        <div class="number">04</div>
-        <div class="body">
-          <div class="location">Wan Chai, Hong Kong</div>
-          <div class="headline">Photo by Sean Foley</div><a href="https://unsplash.com/photos/aPDCEoW7B78" target="_blank">
-            <div class="link">View on Unsplash</div></a>
-        </div>
-      </div>
-      <div class="slide" style="background-image: url(https://alca.tv/static/u/b9669f2a-17fc-47dc-b6b5-3f863004efa8_opt.png);">
-        <div class="number">05</div>
-        <div class="body">
-          <div class="location">Shibuya-ku, Japan</div>
-          <div class="headline">Photo by Alex Knight</div><a href="https://unsplash.com/photos/Akz00I_GGjU" target="_blank">
-            <div class="link">View on Unsplash</div></a>
-        </div>
-      </div>
-      <div class="slide" style="background-image: url(https://alca.tv/static/u/31979576-5060-4513-aae2-b379b87e7fe6_opt.png);">
-        <div class="number">06</div>
-        <div class="body">
-          <div class="location">Tokyo, Japan</div>
-          <div class="headline">Photo by Benjamin Hung</div><a href="https://unsplash.com/photos/pTn26knnKVw" target="_blank">
-            <div class="link">View on Unsplash</div></a>
-        </div>
-      </div>
-      <div class="slide" style="background-image: url(https://alca.tv/static/u/429b83b8-1ad4-4450-b0de-0a0c1073cf1e_opt.jpg);">
-        <div class="number">07</div>
-        <div class="body">
-          <div class="location">Taipei City, Taiwan</div>
-          <div class="headline">Photo by Jesus In Taiwan</div><a href="https://unsplash.com/photos/v63B_MUiFw8" target="_blank">
-            <div class="link">View on Unsplash</div></a>
-        </div>
-      </div>
+  <div class="body-text">
+    <div class="wrapper">
+      <h1>Shrinking Nav</h1>
+      <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
+        drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
+        locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
+        banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
+        cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
+        irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack
+        wolf, tilde keytar dreamcatcher stumptown seitan truffaut.</p>
+
+      <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
+        drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
+        locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
+        banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
+        cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
+        irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack
+        wolf, tilde keytar dreamcatcher stumptown seitan truffaut.</p>
+
+      <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
+        drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
+        locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
+        banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
+        cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
+        irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack
+        wolf, tilde keytar dreamcatcher stumptown seitan truffaut.</p>
+
+      <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
+        drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
+        locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
+        banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
+        cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
+        irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack
+        wolf, tilde keytar dreamcatcher stumptown seitan truffaut.</p>
     </div>
-    <div id="next-catch"></div>
-    <div id="prev-catch"></div>
   </div>
-  <div id="footer"><a href="" target="_blank">
-      <div id="dribbble"></div></a></div>
-</div>
- 
-</div>
-<div class="body-text">
-  <div class="wrapper">
-    <h1>Shrinking Nav</h1>
-    <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
-      drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
-      locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
-      banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
-      cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
-      irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack wolf,
-      tilde keytar dreamcatcher stumptown seitan truffaut.</p>
-
-    <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
-      drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
-      locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
-      banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
-      cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
-      irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack wolf,
-      tilde keytar dreamcatcher stumptown seitan truffaut.</p>
-
-    <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
-      drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
-      locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
-      banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
-      cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
-      irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack wolf,
-      tilde keytar dreamcatcher stumptown seitan truffaut.</p>
-
-    <p>Viral wayfarers offal vegan, gentrify kinfolk bespoke. Cronut tattooed typewriter, locavore PBR&B man braid authentic
-      drinking vinegar skateboard chartreuse normcore tilde vice VHS. Keffiyeh pinterest everyday carry, selfies YOLO sartorial
-      locavore slow-carb chambray pug echo park green juice kickstarter church-key. Deep v ethical occupy, artisan quinoa
-      banjo banh mi gluten-free food truck hammock. Forage taxidermy pork belly, intelligentsia thundercats ennui chambray
-      cardigan tattooed everyday carry narwhal direct trade. Seitan polaroid waistcoat tofu, post-ironic four dollar toast
-      irony marfa you probably haven't heard of them typewriter stumptown photo booth humblebrag. Distillery fanny pack wolf,
-      tilde keytar dreamcatcher stumptown seitan truffaut.</p>
-  </div>
-</div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="home.js"></script>
-<script src="eventscroll.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="home.js"></script>
+  <script src="eventscroll.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </body>
 
 </html>
