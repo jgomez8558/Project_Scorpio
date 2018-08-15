9a1d3d0bffe594d58f1adad1a577f805238f1957
Unknown <seanfoytek@gmail.com>
2018-08-15 10:57:15 -0500

    Auto stash before merge of "class" and "master"

--- class.php
+++ class.php
@@ -8,24 +8,27 @@ include('connection.php');
 
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
+  <title>Class</title>
+
+ 
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
   <link rel="stylesheet" type="text/css" href="home.css">
   <link rel="stylesheet" type="text/css" href="navbar.css">
   <link rel="stylesheet" type="text/css" href="event.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <script src="class.js"></script>
+
 </head>
+
 <body>
-  
-<nav>
+  <nav>
     <ul>
       <li>
         <a href="/braintrendy/logout.php">Logout</a>
+      </li>
+      <li>
+        <a href="/braintrendy/home.php">Home</a>
       </li>
       <li>
         <a href="/braintrendy/class.php">Classes</a>
@@ -36,61 +39,166 @@ include('connection.php');
     </ul>
   </nav>
 
-
-
-
-<section id="carousel">
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-            <a onclick="window.open(this.href,this.target);return false;" href="profile.php" target="profile"><img class="d-block w-100" src="https://i.ytimg.com/vi/quRy-4K7EPE/maxresdefault.jpg" alt="First slide"></a>
-        </div>
-        <div class="carousel-item">
-            <a onclick="window.open(this.href,this.target);return false;" href="profile.php" target="profile"><img class="d-block w-100" src="http://i.imgur.com/XljCklG.jpg" alt="Second slide"></a>
-        </div>
-        <div class="carousel-item">
-            <a onclick="window.open(this.href,this.target);return false;" href="profile.php" target="profile"><img class="d-block w-100" src="https://i.ytimg.com/vi/Dq87XqrpHFA/maxresdefault.jpg" alt="Third slide"></a>
+  
+  <div id="body">
+    <div id="hero-slides">
+      <div id="header">
+        <div id="menu">
+          <div id="PlayDemo">
+            <div class="bun"></div>
+            <div class="patty"></div>
+            <div class="crisp"></div>
+          </div>
         </div>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-  </section>  
-  <div class="container">
-    
-  <div class="uk-container uk-container-medium uk-padding">
-  <div class="uk-card uk-card-default uk-card-hover uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
-    
-    <div class="uk-card-media-left uk-cover-container">
-      <img src="http://codepen.vincebrown.me/assets/images/snow-mountains.jpg" alt="" uk-cover>
-      <canvas width="600" height="400"></canvas>
-    </div>
-    <div>
-      <div class="uk-card-body">
-        <h3 class="uk-card-title">Media Left</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+      <div id="slides-cont">
+        <div class="button" id="next"></div>
+        <div class="button" id="prev"></div>
+        <div id="slides">
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/ef5af971-fc46-4ee1-a589-980346478696_opt.png);">
+            <div class="number">01</div>
+            <div class="body">
+              <div class="location">Shibuya, Japan</div>
+              <div class="headline">Photo by Benjamin Hung</div>
+              <a href="https://unsplash.com/photos/EYmhcdGuYmI" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/522d6a86-0dcf-4554-8b22-7655d65a1f66_opt.png);">
+            <div class="number">02</div>
+            <div class="body">
+              <div class="location">Mong Kok, Hong Kong</div>
+              <div class="headline">Photo by Ryan Tang</div>
+              <a href="https://unsplash.com/photos/ANJHXftvvJ8" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/9b5f71cb-34a3-4fb4-a3a6-0ce9557c0acd_opt.png);">
+            <div class="number">03</div>
+            <div class="body">
+              <div class="location">Incheon, South Korea</div>
+              <div class="headline">Photo by Steve Roe</div>
+              <a href="https://unsplash.com/photos/73aocAAt7rs" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/10cfd52d-e217-41e9-bb8b-ad43384a5e63_opt.png);">
+            <div class="number">04</div>
+            <div class="body">
+              <div class="location">Wan Chai, Hong Kong</div>
+              <div class="headline">Photo by Sean Foley</div>
+              <a href="https://unsplash.com/photos/aPDCEoW7B78" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/b9669f2a-17fc-47dc-b6b5-3f863004efa8_opt.png);">
+            <div class="number">05</div>
+            <div class="body">
+              <div class="location">Shibuya-ku, Japan</div>
+              <div class="headline">Photo by Alex Knight</div>
+              <a href="https://unsplash.com/photos/Akz00I_GGjU" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/31979576-5060-4513-aae2-b379b87e7fe6_opt.png);">
+            <div class="number">06</div>
+            <div class="body">
+              <div class="location">Tokyo, Japan</div>
+              <div class="headline">Photo by Benjamin Hung</div>
+              <a href="https://unsplash.com/photos/pTn26knnKVw" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+          <div class="slide" style="background-image: url(https://alca.tv/static/u/429b83b8-1ad4-4450-b0de-0a0c1073cf1e_opt.jpg);">
+            <div class="number">07</div>
+            <div class="body">
+              <div class="location">Taipei City, Taiwan</div>
+              <div class="headline">Photo by Jesus In Taiwan</div>
+              <a href="https://unsplash.com/photos/v63B_MUiFw8" target="_blank">
+                <div class="link">View on Unsplash</div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div id="next-catch"></div>
+        <div id="prev-catch"></div>
+      </div>
+      <div id="footer">
+        <a href="" target="_blank">
+          <div id="dribbble"></div>
+        </a>
       </div>
     </div>
+
   </div>
+  <div class="container-fluid">
 
+  <div class="col">
+  <div class="row">
+  
+    ` <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="https://secure.i.telegraph.co.uk/multimedia/archive/01358/students_classroom_1358062c.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">This is a class for learning javascript, html, and css.</p>
+        <p>Professer : Farnsworth</p>
+        <a href="classinfo.php" class="btn btn-primary">Classinfo</a>
+      </div>
+    </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="https://secure.i.telegraph.co.uk/multimedia/archive/01358/students_classroom_1358062c.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">This is a class for learning javascript, html, and css.</p>
+        <p>Professer : Farnsworth</p>
+        <a href="classinfo.php" class="btn btn-primary">Classinfo</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="https://secure.i.telegraph.co.uk/multimedia/archive/01358/students_classroom_1358062c.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">This is a class for learning javascript, html, and css.</p>
+        <p>Professer : Farnsworth</p>
+        <a href="classinfo.php" class="btn btn-primary">Classinfo</a>
+      </div>
+    </div>
+
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="https://secure.i.telegraph.co.uk/multimedia/archive/01358/students_classroom_1358062c.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text">This is a class for learning javascript, html, and css.</p>
+        <p>Professer : Farnsworth</p>
+        <a href="classinfo.php" class="btn btn-primary">ClassInfo</a>
+      </div>
+    </div>
+
+  </div>
+ </div>
+
+ 
+</div>
+
+</div>
+      
+    
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="class.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="home.js"></script>
+  <script src="eventscroll.js"></script>
   <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-
- </body>
+</body>
 
 </html>
